73d19ae658e34c634855e9d9ab5cdee2a7cd7612
pooyan <pooyan.azadparvar@gmail.com>
2020-11-02 08:23:47 +0330

    Update prime-algorithm.py
    completed

--- prime-algorithm.py
+++ prime-algorithm.py
@@ -1,9 +1,12 @@
 n=int(input("Enter your number: "))
-l = []
-for i in range (2,n):
-    if n%i==0:
-        l.append(i)
-if len(l) == 0 :
-    print("Its Prime")
+if n==1 or n==2:
+	print("Its Prime")
 else:
-    print("its devided by " + str(l))
+	l = []
+	for i in range (2,n):
+	    if n%i==0:
+	        l.append(i)
+	if len(l) == 0 :
+	    print("Its Prime")
+	else:
+	    print("its devided by " + str(l))
