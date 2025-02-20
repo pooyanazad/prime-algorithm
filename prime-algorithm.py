edf9929e44f6574a111af8010e46986f879e39e0
Pooyan <pooyan.azadparvar@gmail.com>
2025-02-20 01:50:36 +0100

    Update prime-algorithm.py

--- prime-algorithm.py
+++ prime-algorithm.py
@@ -1,13 +1,9 @@
 #just another way to find prime numbers, If list empty its prime
-n=int(input("Enter your number: "))
-if n==2:
-	print("Its Prime")
-else:
-	l = []
-	for i in range (2,n):
-	    if n%i==0:
-	        l.append(i)
-	if len(l) == 0 :
-	    print("Its Prime")
-	else:
-	    print("its devided by " + str(l))
+def is_prime(num):
+    """Return True if `num` is prime, otherwise False."""
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
